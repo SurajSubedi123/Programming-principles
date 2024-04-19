68dd4e33ea6c7ba89b88387705788ebde1ebbb8b
SurajSubedi123 <165862541+SurajSubedi123@users.noreply.github.com>
2024-04-19 17:49:05 +1200

    Update project.py

--- project.py
+++ project.py
@@ -2,16 +2,13 @@ import turtle
 import random
 
 # Set up the screen
-#  It normally starts by importing the necessary module called Turtle, which allows us to create graphics. 
-#Then, it sets up the game screen, giving it a size of 600x600 pixels with a white background.
+
 screen = turtle.Screen()
 screen.setup(width=600, height=600)
 screen.title("Turtle Game")
 screen.bgcolor("white")
 
 # Create the player turtle
-# A turtle is created to represent the player's character. The turtle is positioned in the middle of the screen. 
-#And the turtle player is located with a blue color.
 
 player = turtle.Turtle()
 player.shape("turtle")
@@ -20,7 +17,7 @@ player.penup()
 player.speed(0)
 
 #Creating the coin
-# A yellow circle is created to represent a coin. It is placed at a random position on the screen.
+
 coin = turtle.Turtle()
 coin.shape("circle")
 coin.color("yellow")
@@ -29,8 +26,7 @@ coin.speed(0)
 coin.goto(random.randint(-290, 290), random.randint(-290, 290))
 
 # Create the obstacles
-#Five red squares are created to represent obstacles. 
-#They are placed at random positions on the screen.
+
 obstacles = []
 for _ in range(5):
     obstacle = turtle.Turtle()
@@ -59,7 +55,7 @@ def move_right():
     player.setx(x + 20)
 
 # Keyboard bindings
-#The game listens for keyboard input and calls the corresponding movement functions when arrow keys are pressed.
+
 screen.listen()
 screen.onkeypress(move_up, "Up")
 screen.onkeypress(move_down, "Down")
@@ -67,9 +63,7 @@ screen.onkeypress(move_left, "Left")
 screen.onkeypress(move_right, "Right")
 
 # Main game loop
-#The game runs in an infinite loop. if the player turtle has collected the coin, in which case the coin moves to a new position. 
-# also checks for collisions between the player turtle and obstacles, resetting the new position to the center. 
-# if a collision occurs then the game screen is updated to reflect any changes.
+
 while True:
     # Move the coin if player collects it
     if player.distance(coin) < 20:
@@ -81,7 +75,3 @@ while True:
             player.goto(0, 0)  # Reset player position
     
     screen.update()
-
-#refrences: https://www.w3schools.com/python/default.asp
-#https://www.python.org/about/gettingstarted
-#https://www.geeksforgeeks.org/python-programming-language
